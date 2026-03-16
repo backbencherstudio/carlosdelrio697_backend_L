1dacb05e8bf8b6add83f6b128f3aebb398a6b7ab
sahadat96 <sahadatappdev>
2026-03-16 13:24:38 +0600

    some update in settings table

--- database/migrations/2026_03_16_055404_create_settings_table.php
+++ database/migrations/2026_03_16_055404_create_settings_table.php
@@ -13,12 +13,18 @@ return new class extends Migration
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
             $table->string('company_name')->nullable();
             $table->string('support_mail')->nullable();
             $table->boolean('email_on_new_orders')->nullable()->default(false);
             $table->boolean('email_on_compliance_flags')->nullable()->default(false);
             $table->boolean('email_on_risk_allert')->nullable()->default(false);
             $table->timestamps();
+
+            $table->unique('user_id');
         });
     }
 
